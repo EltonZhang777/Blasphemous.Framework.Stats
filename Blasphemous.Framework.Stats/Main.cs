86744ab96dd3dceadc1fa355861fa44a1a0c3f83
EltonZhang777 <280903158@qq.com>
2024-11-11 16:25:41 -0500

    Initial commit

---
+++ Blasphemous.Framework.Stats/Main.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+
+namespace Blasphemous.Framework.Stats;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "2.4.1")]
+[BepInDependency("Blasphemous.Framework.UI", "0.1.2")]
+
+public class Main : BaseUnityPlugin
+{
+    public static StatsFramework StatsFramework { get; private set; }
+
+    private void Start()
+    {
+        StatsFramework = new StatsFramework();
+    }
+}
---
+++ Blasphemous.Framework.Stats/StatsFramework.cs
@@ -0,0 +1,14 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.Framework.Stats
+{
+    public class StatsFramework : BlasMod
+    {
+        public StatsFramework() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+        protected override void OnInitialize()
+        {
+            LogError($"{ModInfo.MOD_NAME} has been initialized");
+        }
+    }
+}
